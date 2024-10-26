3ecd7391ca3471ca3e7d8b2f80c5f272278a8903
Eashley Martínez <113074720+eashpie7u7@users.noreply.github.com>
2024-10-26 01:54:16 -0600

    Changes in the style of Education component

--- scam-monitor-front/src/routes/Education.jsx
+++ scam-monitor-front/src/routes/Education.jsx
@@ -1,7 +1,9 @@
 import '../styles/education.css';
 import { useNavigate } from 'react-router-dom';
 import NavBar from "../components/Navbar";
-
+import pishing from "../images/phishing.png"
+import fakecall from  "../images/fakecall.png"
+import gift from "../images/gift.png"
 function Education() {
   const navigate = useNavigate();
 
@@ -15,20 +17,23 @@ function Education() {
         style={{ cursor: 'pointer' }}
       >
         <p className="education-title">Phishing</p>
+        <img className="icon-image" src={pishing} alt="Call icon" />
       </div>
       <div
         className="education-card"
         onClick={() => navigate('/education/tec_support_calls')} 
         style={{ cursor: 'pointer' }}
       >
-        <p className="education-title">Llamadas de soporte técnico falsas</p>
+        <p className="education-title">Llamadas falsas</p>
+        <img className="fake-image" src={fakecall} alt="Call icon" />
       </div>
       <div
         className="education-card"
         onClick={() => navigate('/education/prices_lotery')}
         style={{ cursor: 'pointer' }}
       >
-        <p className="education-title">Estafas de premios y loterías</p>
+        <p className="education-title">Estafas de premios</p>
+        <img className="icon-image" src={gift} alt="Call icon" />
       </div>
       <NavBar />
     </div>
