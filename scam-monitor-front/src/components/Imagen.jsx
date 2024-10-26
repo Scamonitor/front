4ede6f5f77c8361bae24888bd0e05dafe429cdac
Eashley Martínez <113074720+eashpie7u7@users.noreply.github.com>
2024-10-25 19:17:07 -0600

    First style of the imagen, text and audio files

---
+++ scam-monitor-front/src/components/Imagen.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/imagen.css';
+import imagendos from './images/upload_image.png'; 
+function Imagen() {
+    return (
+      <div className="imagen-container">
+        <div className="imagen-card">
+        <p className="title-image">Analyze card</p>
+      </div>
+      <p>Tapping the blue rectangle will open your gallery. Select the screenshot you took that seems suspicious.</p>
+      <div className="upload-image-card">
+        <imagendos/>
+      </div>
+      </div>
+    );
+  }
+  
+  export default Imagen;
+  
