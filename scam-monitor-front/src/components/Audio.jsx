f9e2e4e77b8852714eb724182c45b5546b3a426a
Carlos Paez <carlosalbertopaez2002@gmail.com>
2024-10-26 02:31:13 -0600

    Fix audio and connect to backend

--- scam-monitor-front/src/components/Audio.jsx
+++ scam-monitor-front/src/components/Audio.jsx
@@ -1,26 +1,91 @@
-import React, { useState } from 'react';
-import { ReactMic } from 'react-mic';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMicrophone, faStop } from '@fortawesome/free-solid-svg-icons';
-import '../styles/audio.css'; // Make sure to create and style this CSS file
+import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone, faStop } from "@fortawesome/free-solid-svg-icons";
+import "../styles/audio.css"; // Make sure to create and style this CSS file
+
+import { MediaRecorder, register } from "extendable-media-recorder";
+import { connect } from "extendable-media-recorder-wav-encoder";
 
 function Audio() {
+  const [audioBlob, setAudioBlob] = useState(null);
+  const [recording, setRecording] = useState(false);
+  const mediaRecorderRef = useRef(null);
+  const audioChunksRef = useRef([]);
+
   const [record, setRecord] = useState(false);
 
-  const toggleRecording = () => {
-    setRecord(prevRecord => !prevRecord);
-  };
+  async function enableStream() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+      await register(await connect());
+      mediaRecorderRef.current = new MediaRecorder(stream, {
+        mimeType: "audio/wav",
+      });
 
-  const onStop = (recordedBlob) => {
-    console.log('Recorded Blob:', recordedBlob);
-    // Here you can send the recordedBlob to your backend
+      mediaRecorderRef.current.ondataavailable = (event) => {
+        audioChunksRef.current.push(event.data);
+      };
 
-    // Verify the MIME type of the recordedBlob
-    if (recordedBlob.blob.type === 'audio/wav') {
-      console.log('The MIME type of the recordedBlob is audio/wav');
-    } else {
-      console.log('The MIME type of the recordedBlob is not audio/wav');
+      mediaRecorderRef.current.onstop = async () => {
+        const blob = new Blob(audioChunksRef.current, {
+          type: "audio/wav",
+        });
+        setAudioBlob(blob);
+        uploadRecording(blob);
+        const bf = await blob.arrayBuffer();
+        console.log(bf);
+        audioChunksRef.current = [];
+      };
+    } catch (err) {
+      console.error("Error accessing the microphone", err);
     }
+  }
+
+  useEffect(() => {
+    enableStream();
+    return () => {
+      if (mediaRecorderRef.current) {
+        mediaRecorderRef.current.stream
+          .getTracks()
+          .forEach((track) => track.stop());
+      }
+    };
+  }, []);
+
+  function startRecording() {
+    if (!mediaRecorderRef.current) return;
+    audioChunksRef.current = [];
+    mediaRecorderRef.current.start();
+    setRecording(true);
+  }
+
+  function stopRecording() {
+    if (!mediaRecorderRef.current) return;
+    mediaRecorderRef.current.stop();
+    setRecording(false);
+  }
+
+  const uploadRecording = (blob) => {
+    const formData = new FormData();
+    console.log("BLOB", blob);
+    formData.append("audio_file", blob, "record-audio.wav"); // The third argument is the file name
+    formData.append("type", "AUDIO"); // The user ID
+    fetch("http://127.0.0.1:5000/reports", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json(); // Assuming the backend responds with JSON
+      })
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
@@ -30,21 +95,21 @@ function Audio() {
         <FontAwesomeIcon icon={faMicrophone} size="2x" color="#ffffff" />
       </div>
       <p className="audio-description">
-        Click the button below to start recording your audio. Once finished, click stop to analyze.
+        Click the button below to start recording your audio. Once finished,
+        click stop to analyze.
       </p>
       <div className="sound-wave-container">
-        <ReactMic
-          record={record}
-          className="sound-wave"
-          onStop={onStop}
-          mimeType="audio/wav"
-          strokeColor="#000000"
-          backgroundColor="#BAD3F8"
-        />
+        <button onClick={recording ? stopRecording : startRecording}>
+          hiii
+        </button>
       </div>
       <div className="audio-controls">
-        <button onClick={toggleRecording} type="button" className="icon-button">
-          <FontAwesomeIcon icon={record ? faStop : faMicrophone} />
+        <button
+          onClick={recording ? stopRecording : startRecording}
+          type="button"
+          className="icon-button"
+        >
+          <FontAwesomeIcon icon={recording ? faStop : faMicrophone} />
         </button>
       </div>
     </div>
