eadcd1fe0bee44a83ccdee359b937abeca325383
Jeronimo <A01644050@tec.mx>
2024-10-26 07:31:31 -0600

    login resouerce added plus carousel

--- scam-monitor-front/src/App.jsx
+++ scam-monitor-front/src/App.jsx
@@ -5,12 +5,7 @@ import Image from "./components/Imagen.jsx"
 import Audio from "./components/Audio.jsx"
 import Profile from "./routes/Profile.jsx";
 import Education from "./routes/Education.jsx";
-import Phishing from "./components/Phishing.jsx";
 import ImageResponse from "./components/ImageResponse.jsx";
-import FakeCall from "./components/FakeCall.jsx"
-import ExampleComponent from "./components/ExampleComponent.jsx";
-import PricesLotery from "./components/PricesLotery.jsx";
-import PhishingTest from "./components/PhishingTest.jsx";
 
 function App() {
   return (
@@ -18,20 +13,13 @@ function App() {
       <div className="App">
         <div className="App-content">
           <Routes>
-            <Route path="/" element={}></Route>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/fraud" element={<Fraud />} />
             <Route path="/fraud/image" element={<Image />} />
             <Route path="/fraud/audio" element={<Audio />} />
             <Route path="/profile" element={<Profile />} /> 
             <Route path="/education" element={<Education />} />
-            <Route path="/education/phishing" element={<Phishing />} />
             <Route path="/fraud/image/response" element={<ImageResponse />} />
-            <Route path="/education/tec_support_calls" element={<FakeCall />} />
-            <Route path="/phishing/ejemplos" element={<ExampleComponent />} />
-            <Route path="/education/prices_lotery" element={<PricesLotery />} />
-            <Route path="/phishing/test" element={<PhishingTest />} />
-            
           </Routes>
         </div>
       </div>
